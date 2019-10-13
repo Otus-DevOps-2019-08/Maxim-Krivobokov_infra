88bf42016d0a8f9e58ef0e1b8291336b627b8f2f
Maxim-Krivobokov <maxim.krivobokov@gmail.com>
2019-10-13 19:54:49 +0300

    ansible HW done

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -8,12 +8,16 @@ resource "google_compute_instance" "db" {
   }
   network_interface {
     network = "default"
-    #  access_config = {  }
+    access_config { 
+        }
   }
   metadata = {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 }
+#resource "google_compute_address" "db_ip" {
+#  name = "reddit-db-ip"
+#}
 resource "google_compute_firewall" "firewall_mongo" {
   name    = "allow-mongo-default"
   network = "default"
